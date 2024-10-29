22459b2c61ecb8f7e5e7d41e5e0486f5e839e82b
Grigoris <49034550+grigorisar@users.noreply.github.com>
2024-10-29 16:57:35 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,94 @@
-<script>
-	import Header from './Header.svelte';
-	import '../app.css';
+<script lang="ts">
+  import type { LayoutData } from './$types';
+
+  export let data: LayoutData;
+
+  import Drawer, { AppContent, Content, Header, Title, Subtitle } from '@smui/drawer';
+  import Button, { Label } from '@smui/button';
+  import List, { Item, Text, Separator, Graphic } from '@smui/list';
+
+  let open = true;
+  let active = 'Overview';
+
+  type DrawerItem = 'Overview' | 'Summary';
+
+  function drawerNavigation(value: DrawerItem) {
+    active = value;
+  }
+
+  function handleItemClick(item: string) {
+    console.log(item);
+  }
 </script>
 
-<div class="app">
-	<Header />
+<div class="drawer-container">
+  <Drawer variant="dismissible" bind:open>
+    <Header>
+      <Title>Time Tracker</Title>
+      <Subtitle>Select a view.</Subtitle>
+    </Header>
+    <!-- <Separator /> -->
+    <Content>
+      <List>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => drawerNavigation('Overview')}
+          activated={active === 'Overview'}
+        >
+          <Graphic class="material-icons">send</Graphic>
+          <Text>Overview</Text>
+        </Item>
+        <!-- <Separator /> -->
 
-	<main>
-		<slot />
-	</main>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => drawerNavigation('Summary')}
+          activated={active === 'Summary'}
+        >
+          <Graphic class="material-icons">archive</Graphic>
+          <Text>Summary</Text>
+        </Item>
+      </List>
+    </Content>
+  </Drawer>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+  <AppContent class="app-content">
+    <Button on:click={() => (open = !open)}>
+      <Label>Toggle Drawer</Label>
+    </Button>
+    <!-- <div class="main-content"> -->
+    <slot {drawerNavigation} />
+    <!-- </div> -->
+  </AppContent>
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
+  :global(.mdc-drawer) {
+    height: inherit;
+  }
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  :global(.mdc-deprecated-list) {
+    /* height: 100%; */
+    display: flex;
+    flex-direction: column;
+  }
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
+  * :global(.app-content) {
+    flex: auto;
+    overflow: auto;
+    position: relative;
+    flex-grow: 1;
+  }
 
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
+  /* These classes are only needed because the
+    drawer is in a container on the page. */
+  .drawer-container {
+    position: relative;
+    display: flex;
+    height: calc(100vh - 154px);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+    border: 1px solid var(--mdc-theme-text-hint-on-background, rgba(0, 0, 0, 0.1));
+    overflow: hidden;
+    z-index: 0;
+  }
 </style>
